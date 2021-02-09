b1c74107ec843adabfea2e4f889a41b6fe2853b1
unknown <ivcbibl@VLAN-25-BI-164.rd.mirea.ru>
2021-02-09 14:05:54 +0300

    init

---
+++ task1/t1_1.py
@@ -0,0 +1,14 @@
+import math
+
+
+def function1_1(x, y, z):
+    return (25*z + math.exp(z))/(pow(z, 8) - x)\
+           - (abs(pow(x, 5) - 46*pow(x, 8)) - 13*pow(x, 6))\
+           - math.sqrt((90*z + pow(x, 3))/((pow(z, 6)/45) + 8*pow(y, 5) - 2))
+
+
+
+x = float(input())
+y = float(input())
+z = float(input())
+print(function1_1(x, y, z))
---
+++ task1/t1_2.py
@@ -0,0 +1,19 @@
+import math
+
+
+def function1_2(x):
+    if x < 85:
+        value = 25*(math.cos(x) - 7*pow(x, 7)) + pow(x, 7)
+    elif x < 110:
+        value = pow(x, 4) + pow(x, 6)
+    elif x < 186:
+        value = 36*pow(x, 7) + pow(x, 5) - 84
+    elif x < 202:
+        value = pow((x - 41*pow(x,2)), 4) + math.tan(x)
+    elif x >= 202:
+        value = math.tan(x) + math.log(x)
+    return value
+
+
+x = float(input())
+print(function1_2(x))
