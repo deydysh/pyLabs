161189218df298f60b8ab88e250cfeb67e0ec9ab
Ilya <ily-1610@rambler.ru>
2021-02-15 19:12:07 +0200

    fix 3rd

--- task1/t1_3.py
+++ task1/t1_3.py
@@ -2,16 +2,16 @@ import math
 
 
 def function1_3(n, m):
-    value1, value2 = 0
-    for i in range(1, n):
+    value1, value2 = 0, 0
+    for i in range(1, n+1):
         value1 += 25*i + math.exp(i)
-    for i in range(1, n):
-        for j in range(1, m):
-            value2 = pow(i, 3) + pow(j, 4) + 82
+    for j in range(1, n+1):
+        for k in range(1, m+1):
+            value2 += pow(j, 3) + pow(k, 4) + 82
     value = value1 - 2*value2
     return value
 
 
-n = float(input())
-m = float(input())
-print(function1_3(m, n))
+n = int(input())
+m = int(input())
+print(function1_3(n, m))
